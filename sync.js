304fb97ceeb5a367aa2d312ebabbd230cf2aef91
ngriaznov <ngriaznov@janusdisplays.com>
2020-09-13 18:14:43 +0300

    Files are updated using move

--- sync.js
+++ sync.js
@@ -1,39 +1,27 @@
+const { v4: uuidv4 } = require('uuid');
+
 const IPFS = require("ipfs");
 var fs = require("fs-extra");
 const path = require("path");
 const chokidar = require("chokidar");
-let rootCid = "";
 
-async function readConfig() {
-  if (await fs.pathExists("./config.json")) {
-    return await fs.readJson("./config.json");
-  }
-  return [];
-}
+let ipfs;
 
-async function writeConfig(config) {
-  await fs.writeJson("./config.json", config);
-}
-
-async function upsertFile(ipfs, name, contents) {
-  let create = false;
-
+async function upsertFile(name, contents) {
+  const id = uuidv4();
+  const uploadPath = `/tmp/${id}`;
+  await ipfs.files.write(uploadPath, contents, { create: true, parents: true });
   try {
     await ipfs.files.stat(name);
-  } catch {
-    create = true;
-  }
-
-  if (!create) {
     await ipfs.files.rm(name);
+  } catch {
   }
 
-  await ipfs.files.write(name, contents, {
-    create: create,
-    parents: create,
-  });
+  await ipfs.files.cp(uploadPath, name);
+  await ipfs.files.rm(uploadPath);
 
-  console.log(`Added or updated file: ${name}.`);
+  const rstats = await ipfs.files.stat('/');
+  console.log(rstats.cid.string);
 }
 
 async function deleteFile(ipfs, name) {
@@ -43,68 +31,30 @@ async function deleteFile(ipfs, name) {
 async function initIpfs(directory) {
   await fs.ensureDir(directory);
 
-  // Create IPFS
-  const ipfs = await IPFS.create({ silent: true });
-  const rootName = path.basename(directory);
+  ipfs = await IPFS.create({ silent: true, repo: "./sync" });
 
-  // Create root folder and print CID
-  const config = await readConfig();
-  const rootConfig = config.find((c) => c.root == directory);
-  if (rootConfig) {
-    rootCid = rootConfig.cid;
-  } else {
-    try {
-      await ipfs.files.mkdir(`/${rootName}`);
-    } catch {
-      // Ignore if folder exists
-    }
+  const root = path.basename(directory);
+  const stats = await ipfs.files.stat('/');
+  console.log(stats.cid.string);
 
-    let root = await ipfs.files.stat(`/${rootName}`);
-    rootCid = root.cid.string;
-    config.push({ root: directory, cid: rootCid });
-    writeConfig(config);
-  }
-
-  console.log(rootCid);
-
-  chokidar.watch(directory).on(
-    "add",
-    async (filePath) => {
-      const relativeName = path.relative(directory, filePath);
-      await upsertFile(
-        ipfs,
-        path.join(`/ipfs/${rootCid}`, relativeName),
-        fs.readFileSync(filePath)
-      );
-    },
-    {
-      ignored: (path) => path.includes("DS_Store"),
-    }
-  );
-  chokidar.watch(directory).on(
-    "unlink",
-    async (filePath) => {
-      const relativeName = path.relative(directory, filePath);
-      await deleteFile(ipfs, path.join(`/ipfs/${rootCid}`, relativeName));
-    },
-    {
-      ignored: (path) => path.includes("DS_Store"),
-    }
-  );
-  chokidar.watch(directory).on(
-    "change",
-    async (filePath) => {
-      const relativeName = path.relative(directory, filePath);
-      await upsertFile(
-        ipfs,
-        path.join(`/ipfs/${rootCid}`, relativeName),
-        fs.readFileSync(filePath)
-      );
-    },
-    {
-      ignored: (path) => path.includes("DS_Store"),
-    }
-  );
+  chokidar.watch(directory).on("add", async (filePath) => {
+    const relativeName = path.relative(directory, filePath);
+    await upsertFile(
+      `/${root}/${relativeName}`,
+      fs.readFileSync(filePath)
+    );
+  });
+  chokidar.watch(directory).on("unlink", async (filePath) => {
+    const relativeName = path.relative(directory, filePath);
+    await deleteFile(`/${root}/${relativeName}`);
+  });
+  chokidar.watch(directory).on("change", async (filePath) => {
+    const relativeName = path.relative(directory, filePath);
+    await upsertFile(
+      `/${root}/${relativeName}`,
+      fs.readFileSync(filePath)
+    );
+  });
 }
 
 const args = process.argv.slice(2);
