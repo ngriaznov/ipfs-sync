e5de8dbd1bc3b56a1236b80e225b395dfc76ee65
ngriaznov <ngriaznov@janusdisplays.com>
2020-09-13 22:17:02 +0300

    Update

--- sync.js
+++ sync.js
@@ -1,41 +1,64 @@
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const IPFS = require("ipfs");
+const OrbitDB = require("orbit-db");
 var fs = require("fs-extra");
 const path = require("path");
 const chokidar = require("chokidar");
 
 let ipfs;
+let db;
 
-async function upsertFile(name, contents) {
+async function upsertFile(root, name, contents) {
+  name = `/${root}/${name}`;
+
   const id = uuidv4();
   const uploadPath = `/upload/${id}`;
+
   await ipfs.files.write(uploadPath, contents, { create: true, parents: true });
   try {
     await ipfs.files.stat(name);
     await ipfs.files.rm(name);
-  } catch {
-  }
+  } catch {}
 
   await ipfs.files.cp(uploadPath, name, { parents: true });
   await ipfs.files.rm(uploadPath);
 
-  const rstats = await ipfs.files.stat('/');
+  const rstats = await ipfs.files.stat("/");
   console.log(rstats.cid.string);
+  await db.put(root, rstats.cid.string);
 }
 
-async function deleteFile(name) {
+async function deleteFile(root, name) {
   await ipfs.files.rm(name);
+  const rstats = await ipfs.files.stat("/");
+
+  console.log(rstats.cid.string);
+  await db.put(root, rstats.cid.string);
 }
 
 async function initIpfs(directory) {
-  await fs.ensureDir(directory);
-
   ipfs = await IPFS.create({ silent: true, repo: "./sync" });
 
+  // Init Orbit
+  const orbitdb = await OrbitDB.createInstance(ipfs);
+
+  // Create / Open a database
+  db = await orbitdb.keyvalue("distribution", { sync: true });
+  await db.load();
+
+  console.log(`Orbit: ${db.address.toString()}`);
+
+  // Listen for updates from peers
+  db.events.on("replicated", (address) => {
+    console.log(db.iterator({ limit: -1 }).collect());
+  });
+
+  await fs.ensureDir(directory);
+
   const root = path.basename(directory);
-  const stats = await ipfs.files.stat('/');
-  const files = await ipfs.files.ls('/');
+  const stats = await ipfs.files.stat("/");
+  const files = await ipfs.files.ls("/");
 
   for await (const file of files) {
     await ipfs.files.rm(`/${file.name}`, { recursive: true });
@@ -45,21 +68,15 @@ async function initIpfs(directory) {
 
   chokidar.watch(directory).on("add", async (filePath) => {
     const relativeName = path.relative(directory, filePath);
-    await upsertFile(
-      `/${root}/${relativeName}`,
-      fs.readFileSync(filePath)
-    );
+    await upsertFile(root, relativeName, fs.readFileSync(filePath));
   });
   chokidar.watch(directory).on("unlink", async (filePath) => {
     const relativeName = path.relative(directory, filePath);
-    await deleteFile(`/${root}/${relativeName}`);
+    await deleteFile(root, relativeName);
   });
   chokidar.watch(directory).on("change", async (filePath) => {
     const relativeName = path.relative(directory, filePath);
-    await upsertFile(
-      `/${root}/${relativeName}`,
-      fs.readFileSync(filePath)
-    );
+    await upsertFile(root, relativeName, fs.readFileSync(filePath));
   });
 }
 
@@ -71,7 +88,5 @@ if (args.length == 0) {
 }
 
 const folder = args[0];
-
-console.log(`Target folder: ${folder}`);
 
 initIpfs(folder);
