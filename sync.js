d488bff7b8a1698ef4f251372f48787ecb49685c
ngriaznov <ngriaznov@janusdisplays.com>
2020-09-14 11:27:37 +0300

    Config update

--- sync.js
+++ sync.js
@@ -102,7 +102,6 @@ async function start() {
   ipfs = await IPFS.create();
 
   config.paths.forEach(async (directory) => {
-    directory = path.resolve(directory);
     console.log(`Add directory: ${directory}`);
     await fs.ensureDir(directory);
 
